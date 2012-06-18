62c5568c88712002cd82646ef30e9875f07cd43b
Yizheng Cai <yizhengc@gmail.com>
2012-06-17 22:14:02 -0700

    Messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  root :to => 'static_pages#home'
+  
+  match '/help' => 'static_pages#help'
 
-  get "static_pages/help"
+  match '/about' => 'static_pages#about'
 
-  get "static_pages/about"
+  match '/contact' => 'static_pages#contact'
 
-  get "static_pages/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
